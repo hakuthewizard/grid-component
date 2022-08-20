836265d218a2341c83a2023c51771c227e6b3ab1
hakuthewizard <hakuthegeek@gmail.com>
2022-08-20 10:59:12 +0200

    update

--- login.js
+++ login.js
@@ -29,6 +29,6 @@ form.addEventListener('submit', (e) => {
 
   if (messages.length > 0) {
     e.preventDefault()
-    errorElement.innerText = messages.join(', ')
+    errorElement.textContent = messages.join(', ')
   }
 })
